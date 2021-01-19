14f640e91f193b3b17b67d9d42fc60954d896b0c
shivani <shivaniuppal21@gmail.com>
2021-01-19 01:03:04 +0000

    adding headers

--- express_server.js
+++ express_server.js
@@ -1,27 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+//code for body-parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+// tells express to use ejs as templating
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
-  
-  app.get("/urls/:shortURL", (req, res) => {
-      const longurl = urlDatabase[req.params.shortURL]
-    const templateVars = { shortURL: req.params.shortURL, longURL: longurl };
-    res.render("urls_show", templateVars);
-  });
-
 app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+    res.send("Hello!");
+  });
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
@@ -30,8 +22,18 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
+
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
   
-  
+  app.get("/urls/:shortURL", (req, res) => {
+      const longurl = urlDatabase[req.params.shortURL];
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]  };
+    res.render("urls_show", templateVars);
+  });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
