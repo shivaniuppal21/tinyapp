00bf8808483ca9793e75d83476991951dd223244
shivani <shivaniuppal21@gmail.com>
2021-01-20 23:50:14 +0000

    comiting working login logout

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080; // default port 8080
 const morgan = require('morgan'); // HTTP request logger middleware for node.js
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
-const { emailExists, passwordMatching, fetchUser } = require('./helpers/userHelpers')
+const { emailExists, passwordMatching, fetchUser } = require("./helpers/useHelpers")
 
 const app = express();
 app.set("view engine", "ejs"); // tells express to use ejs as templating
@@ -53,14 +53,14 @@ app.get("/hello", (req, res) => {
   });
 
 app.get("/urls", (req, res) => {
-  const username = req.cookies["username"];
-  const templateVars = { urls: urlDatabase,username };
+  const user_id = req.cookies["user_id"];
+  const templateVars = { urls: urlDatabase, user: users[user_id]};
     res.render("urls_index", templateVars);
   });
   
   app.get("/urls/new", (req, res) => {
-    const username = req.cookies["username"];
-    const templateVars = {  username };
+    const user_id = req.cookies["user_id"];
+  const templateVars = {  user: users[user_id]};
     res.render("urls_new",templateVars);
   });
 
@@ -71,10 +71,12 @@ app.get("/urls", (req, res) => {
   });
 
   app.get("/urls/:shortURL", (req, res) => {
-    const username = req.cookies["username"];
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] ,username };
+    const user_id = req.cookies["user_id"];
+    console.log(user_id)
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] ,user: users[user_id] };
     res.render("urls_show", templateVars);
   });
+
  // will show a form to login
   app.get('/register', (req, res) => {
     res.render("register")
@@ -83,24 +85,30 @@ app.get("/urls", (req, res) => {
   app.post("/register", (req, res) => {
     const incomingEmail = req.body.email
     const incomingPassword = req.body.password
-    const incomingName = req.body.name
+    const userid = generateRandomString()
   
+    if (!incomingEmail || !incomingPassword){
+      res.statusCode = 400;
+      res.send('Incorrect username or password');
+      return
+    }
     // We should check if email exists
     if (emailExists(users, incomingEmail)) {
       console.log("email already exists")
-      res.redirect('/register')
+      res.send('email already exists');
+      return
     } else {
       // If not, we want to add the new user data to the databaseish
       const newUser = {
-        name: incomingName,
+        id: userid,
         email: incomingEmail,
         password: incomingPassword
       }
   
-      userDatabaseIsh[incomingEmail] = newUser
+      users[userid] = newUser
   
       // redirect them to home with cookie if everything was ok
-      // res.cookie(incomingEmail)
+      res.cookie("user_id",userid)
       // res.redirect('/')
   
       // redirect them to login if everything was ok
@@ -124,15 +132,34 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls");
 })
 
+app.get("/login", (req, res) => {
+  const user_id = req.cookies["user_id"];
+  const templateVars = { urls: urlDatabase, user: users[user_id]};
+    res.render("login", templateVars);
+
+})
+
+
 app.post("/login", (req, res) => {
 
-  res.cookie("username",req.body.username)
-  console.log(req.body.username)
-  res.redirect("/urls");
+  if(passwordMatching(users,req.body.email,req.body.password))
+  {
+    let user_obj = fetchUser(users,req.body.email)
+    res.cookie("user_id",user_obj.id)
+    console.log(user_obj.id)
+    res.redirect("/urls");
+  }
+  else{
+    res.statusCode = 403;
+      res.send('Error '+res.statusCode +' Username Or password not correct');
+      return
+  }
+  
+  
 })
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
 
--- helpers/useHelpers.js
+++ helpers/useHelpers.js
@@ -1,24 +1,31 @@
 const emailExists = (users, email) => {
-    if (users[email]) {
-      return true
-    } else {
-      return false
+    // fetch user gives user object if email exist
+    if (fetchUser(users,email))
+    {
+        return true
+    }
+    else{
+        return false
     }
   }
+
   const passwordMatching = (users, email, password) => {
-    if (users[email].password === password) {
-      return true
+    let user_obj = fetchUser(users, email)
+    if (user_obj && user_obj.password === password){
+        return true
     } else {
       return false
     }
   }
   
+  // Return user with matching email IF exist - otherwise return null
   const fetchUser = (users, email) => {
-    if (users[email]) {
-      return users[email]
-    } else {
-      return {}
+    for (let userkey in users ){
+        if (users[userkey].email === email) {
+            return users[userkey]
+          } 
     }
+    return null
   }
 
 
