c4b98a9aa5609fc3d3a1e7a15c9557030c9965ab
shivani <shivaniuppal21@gmail.com>
2021-01-24 00:33:38 +0000

    elint on helper

--- helpers/useHelpers.js
+++ helpers/useHelpers.js
@@ -1,65 +1,67 @@
 const bcrypt = require('bcrypt');
 
-// generates a random id 
+/*
+ * This function generates a random id
+ */
+
 function generateRandomString() {
   let result = '';
-  let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charlen = char.length;
+  const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charlen = char.length;
   for ( let i = 0; i < 6; i++ ) {
-     result += char.charAt(Math.floor(Math.random() * charlen));
+    result += char.charAt(Math.floor(Math.random() * charlen));
   }
   return result;
 }
 
 // Checks if given email corresponds to a user in a given database, returns true or false
 const emailExists = (users, email) => {
-    // fetch user gives user object if email exist
-  if (fetchUser(users,email)){
-    return true
+  // fetch user gives user object if email exist
+  if (fetchUser(users, email)) {
+    return true;
   } else {
-    return false
+    return false;
   }
 };
 
 
-  const passwordMatching = (users, email, password) => {
-    let user_obj = fetchUser(users, email)
-    if (user_obj && bcrypt.compareSync(password,user_obj.password)){
-        return true
-    } else {
-      return false
+const passwordMatching = (users, email, password) => {
+  const user_obj = fetchUser(users, email);
+  if (user_obj && bcrypt.compareSync(password, user_obj.password)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Return user with matching email IF exist - otherwise return null
+const fetchUser = (users, email) => {
+  for (const userkey in users ) {
+    if (users[userkey].email === email) {
+      return users[userkey];
     }
   }
-  
-  // Return user with matching email IF exist - otherwise return null
-  const fetchUser = (users, email) => {
-    for (let userkey in users ){
-        if (users[userkey].email === email) {
-            return users[userkey]
-          } 
-    }
-    return null
-  }
+  return null;
+};
 
-  // func takes urlDatabase and userId as its arguments
-  // it returns a subset of urldatabase which matches the userId 
-  // if no userid is matched return null
-const getUrlDatabaseFromUserId = function(urlDatabase, userId){
-  let returnUrlObject = {}
-  for(let urls in urlDatabase){
-    if(urlDatabase[urls].userID === userId){
-      returnUrlObject[urls] = {}
-      returnUrlObject[urls].longURL = urlDatabase[urls].longURL
-      returnUrlObject[urls].userID = urlDatabase[urls].userID
+// func takes urlDatabase and userId as its arguments
+// it returns a subset of urldatabase which matches the userId
+// if no userid is matched return null
+const getUrlDatabaseFromUserId = function(urlDatabase, userId) {
+  const returnUrlObject = {};
+  for (const urls in urlDatabase) {
+    if (urlDatabase[urls].userID === userId) {
+      returnUrlObject[urls] = {};
+      returnUrlObject[urls].longURL = urlDatabase[urls].longURL;
+      returnUrlObject[urls].userID = urlDatabase[urls].userID;
     }
   }
-if(Object.keys(returnUrlObject).length === 0 ){
-  return null
-}else{
-  return returnUrlObject
-}
-}
+  if (Object.keys(returnUrlObject).length === 0 ) {
+    return null;
+  } else {
+    return returnUrlObject;
+  }
+};
 
 
-
-  module.exports = { emailExists, passwordMatching, fetchUser, generateRandomString, getUrlDatabaseFromUserId}
+module.exports = {emailExists, passwordMatching, fetchUser, generateRandomString, getUrlDatabaseFromUserId};
